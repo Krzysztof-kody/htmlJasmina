fa9e17e11df869c123e881a4c94ba1f62611fdf1
Marianna <marianna.jucewicz@gmail.com>
2021-01-17 21:16:07 +0100

    Dropown dziala

---
+++ pkt1/script.js
@@ -0,0 +1,13 @@
+function dropdown(){
+    if( document.getElementById("drop").style.display == "block")
+    document.getElementById("drop").style.display="none";
+    else{
+        document.getElementById("drop").style.display="block";
+    }
+}
+
+function drop(){
+
+    document.getElementById("drop").style.display="block";
+    
+}
